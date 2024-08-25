b2292f61efddb72cb406b3a12e48ce231cb54956
Sebastian D'Agostino <sebastian.danieldagostino@gmail.com>
2024-08-25 15:33:50 -0300

    Ejercicio N°3 hallar el numero intermedio

--- Practica_1.py
+++ Practica_1.py
@@ -1,24 +1,18 @@
-'''Realizar una función que se llame relacion() que a partir de dos nros
-realice lo siguiente:
-a. Si el primer nro es mayor que el segundo, devuelve 1
-b. Si el primer nro es menor que el segundo, devuelve -1
-c. Si ambos nros son iguales, devuelve 0 '''
+'''Realizar una función que se llame intermedio() que a partir de dos nros
+devuelva el punto intermedio. Ej. El punto intermedio
+entre 10 y 24 = 17; entre 12 y 50 = 31.'''
+#se crea una funcion
+def intermedio(valor1, valor2):
+    suma = valor1 + valor2
+    division = suma / 2
+    return division
+# se le pide a los usuarios que ingrese valores
+valor1 = int(input("Ingrese un valor: "))
+valor2 = int(input("Ingrese un valor: "))
 
+#variables para mostrar en la impresion de pantalla
+valor_intermedio = intermedio(valor1, valor2)
 
-# se creo una funcion llamada relacion
-def relacion(numero1, numero2):
-    if numero1 > numero2: #si el primero valor es mayor devuelve 1
-        return 1
-    elif numero1 < numero2:#si el primero valor es menor devuelve -1
-        return -1
-    else:                  #si los valores son iguales devuelve 0
-        return 0
-
-numero1 = int(input("Ingrese un valor: ")) #Se el solicita al usuario un valor
-numero2 = int(input("Ingrese un valor: ")) #Se el solicita al usuario un valor
-
-valor = relacion(numero1, numero2) #variable para mostrar en el resultado final
-
-
-print(f"El resultado es: {valor}")  #Imprime el resultado en pantalla
+#se muestran los valores por pantalla
+print(f"el valor intermedio de {valor1} y {valor2} es {valor_intermedio}")
 
