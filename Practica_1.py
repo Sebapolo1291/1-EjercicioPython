e6720c8038c20ddc2f374f314e6923f910406c00
Sebastian D'Agostino <sebastian.danieldagostino@gmail.com>
2024-08-25 15:29:25 -0300

    Ejercicio 2 Relacion

--- Practica_1.py
+++ Practica_1.py
@@ -1,19 +1,24 @@
-''' Realizar una función que se llame area_rectangulo() que devuelva
-el área del rectángulo a partir de su base y altura.'''
+'''Realizar una función que se llame relacion() que a partir de dos nros
+realice lo siguiente:
+a. Si el primer nro es mayor que el segundo, devuelve 1
+b. Si el primer nro es menor que el segundo, devuelve -1
+c. Si ambos nros son iguales, devuelve 0 '''
 
-#se utiliza la palabra DEF para definir como funcion la siguiente variable
-def area_rectangulo (base, altura): #Se escribe la función
-    multiplicacion = base * altura  #Se escribe la operacion
-    return multiplicacion #el return para que devuelva el valor
 
-base = float(input("Ingrese Base del triangulo")) 
-#Se le pide al usuario que ingrese los valores
-#tambien se agrega el tipo de dato
-altura = float(input("Ingrese la Altura del triangulo"))
+# se creo una funcion llamada relacion
+def relacion(numero1, numero2):
+    if numero1 > numero2: #si el primero valor es mayor devuelve 1
+        return 1
+    elif numero1 < numero2:#si el primero valor es menor devuelve -1
+        return -1
+    else:                  #si los valores son iguales devuelve 0
+        return 0
 
-resultado = area_rectangulo (base, altura)
-#se agrego una variable para poder imprimir el resultado en pantalla
+numero1 = int(input("Ingrese un valor: ")) #Se el solicita al usuario un valor
+numero2 = int(input("Ingrese un valor: ")) #Se el solicita al usuario un valor
 
-print(f"El Area del rectángulo es {resultado}")
-#se imprime el resueltado en la pantalla
+valor = relacion(numero1, numero2) #variable para mostrar en el resultado final
+
+
+print(f"El resultado es: {valor}")  #Imprime el resultado en pantalla
 
