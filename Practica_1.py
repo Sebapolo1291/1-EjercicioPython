3423f3717c859cc2b7e77fcbc19ae4d51ef32a5c
Sebastian D'Agostino <sebastian.danieldagostino@gmail.com>
2024-08-25 15:38:12 -0300

    Ejercicio 4 Lista de Pares e Impares

--- Practica_1.py
+++ Practica_1.py
@@ -1,18 +1,27 @@
-'''Realizar una función que se llame intermedio() que a partir de dos nros
-devuelva el punto intermedio. Ej. El punto intermedio
-entre 10 y 24 = 17; entre 12 y 50 = 31.'''
-#se crea una funcion
-def intermedio(valor1, valor2):
-    suma = valor1 + valor2
-    division = suma / 2
-    return division
-# se le pide a los usuarios que ingrese valores
-valor1 = int(input("Ingrese un valor: "))
-valor2 = int(input("Ingrese un valor: "))
+'''Realizar una función que se llame separar() que reciba una lista de nros y
+devuelva dos listas ordenadas. La primera con nros pares y la segunda con
+nros impares.'''
 
-#variables para mostrar en la impresion de pantalla
-valor_intermedio = intermedio(valor1, valor2)
+# se crea la funcion
+def separar(lista):
+    numeros_pares = []
+    numeros_impares = []
 
-#se muestran los valores por pantalla
-print(f"el valor intermedio de {valor1} y {valor2} es {valor_intermedio}")
+    for n in lista: #condicion para saber si los numeros son pares e impares
+        if n % 2 == 0:
+            numeros_pares.append(n) #append agrega un item al final de la lista
+        else:
+            numeros_impares.append(n)#append agrega un item al final de la lista
+
+
+    return sorted(numeros_pares), sorted(numeros_impares)
+#sorted ordena los valores
+#return devuelve valores pares e impares
+
+valores = input("Ingrese una lista de numeros separados por comas (ejemplo:20, 23, 43): ")
+lista = [int(n) for n in valores.split(',')]
+
+#lista = [0,0,0] se utiliza para almacenar varios valores
+#split define el separador que el usuario va a poder utilizar cuando ingrese los valores
+#int(n) que tipo de valor va a utilizar
 
